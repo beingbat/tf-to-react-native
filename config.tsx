48c2a910413fa31be6b90930d88c895e2953bc8b
beingbat <arsalanmalek@arsalanmalek.local>
2023-09-22 02:27:36 +0500

    Model working, but weights don't seem to be correct

---
+++ config.tsx
@@ -0,0 +1,21 @@
+const AppConfig =
+
+{
+    title: "Malaria Test Classifier"
+    , description: `
+    
+This mobile app is developed by 
+- [Arsalan Malek](https://github.com/beingbat)
+
+This app lets you submit a photo of malaria cell and returns the predicted cell category.
+
+The model was developed using the [Malaria Cell Image Dataset](https://www.kaggle.com/datasets/iarunava/cell-images-for-detecting-malaria) and TensorFlow 2.
+    `
+    , imageSize: 50
+    , topK: 1
+    , precision: 2
+
+};
+
+
+export { AppConfig }
